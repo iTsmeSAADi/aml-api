af1b3202a424e4dafb35cb1c2b4359148540bba0
iTsmeSAADi <saadshah269@gmail.com>
2024-02-04 18:02:13 +0500

    updated

--- controllers/manualController.js
+++ controllers/manualController.js
@@ -363,11 +363,7 @@ export const forensicScanRequest = catchAsyncError(async (req, res, next) => {
     }
 
     const allowedTypes = allowedTypesResponse.data.data.document_types.map(type => type.title);
-<<<<<<< HEAD
     
-=======
->>>>>>> 46f5dc6d572748a4e2b4e38bc43e3b30978a6cd8
-
     console.log("allowedTypes", allowedTypes);
 
     // Check if the provided 'tag' matches one of the allowed titles
